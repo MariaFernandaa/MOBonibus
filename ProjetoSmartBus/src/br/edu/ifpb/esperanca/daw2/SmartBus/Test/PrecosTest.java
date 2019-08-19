d3d1ab64c7a17d7da9492d2daa21dcaa6f28b0f8
MariaFernandaa <maria.benjamim@academico.ifpb.edu.br>
2019-08-19 08:38:25 -0300

    19/08

--- ProjetoSmartBus/src/br/edu/ifpb/esperanca/daw2/SmartBus/Test/PrecosTest.java
+++ ProjetoSmartBus/src/br/edu/ifpb/esperanca/daw2/SmartBus/Test/PrecosTest.java
@@ -1,25 +1,46 @@
 package br.edu.ifpb.esperanca.daw2.SmartBus.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 
+import br.edu.ifpb.esperanca.daw2.SmartBus.Service.PrecoService;
 import br.edu.ifpb.esperanca.daw2.SmartBus.entities.Precos;
 
 public class PrecosTest {
-	
+
 	@Test
-	Precos p = new Precos();
-	p.setPreco(0.60);
-	p.setPreco(1.75);
-	p.setPreco(3.50);
-	p.calculaPrecos();
-	assertEquals(70, p.getPrecos());
-	
-	Precos a2 = new Aluno();
-	a2.setNota(2, 70);
-	try {
-	a2.calculaMedia();
-	fail();
-	} catch (FaltaNotaException e) {}
+	public void variacaoPreco() {
+
+		Precos p = new Precos();
+		p.setId(0L);
+		p.setInteira(10);
+		p.setMeia(5);
+		p.setDistância(10);
 		
-	
+		PrecoService service = new  PrecoService();
+		service.save(p);
+		
+		Precos p2 = service.getByID(0L);
+		
+		assertEquals(p2, p);
+		assertEquals(10, p2.getInteira());
+		
+		p2.setInteira(12);
+		p2.setMeia(6);
+		
+		service.update(p2);
+		
+		Precos p3 = service.getByID(0L);
+		
+		assertEquals(12, p3.getInteira());
+		
+		service.remove(p3);
+		
+		Precos p4 = service.getByID(0L);
+		
+		assertNull(p4);
+		
+	}
 }
